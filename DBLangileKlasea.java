8c4a26a966d0b4fd0fd8508815ef2ad0c7b3e9b0
MikelUrlezaga <murlezagap22@fpsanjorge.com>
2023-02-23 13:08:46 +0100

    DB

---
+++ DBLangileKlasea.java
@@ -0,0 +1,11 @@
+public class DBLangileKlasea {
+    private String [] langileak;
+
+    public String [] getLangileak(){
+        return this.langileak;
+     }
+     
+     public void setLangileak(String [] langileak){
+        this.langileak=langileak;
+     }
+}
---
+++ DBProduktuKlasea.java
@@ -0,0 +1,11 @@
+public class DBProduktuKlasea {
+    private String [] produktu;
+
+    public String [] getProduktu(){
+        return this.produktu;
+     }
+     
+     public void setLangileak(String [] produktu){
+        this.produktu=produktu;
+     } 
+}
